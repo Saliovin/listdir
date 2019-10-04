c4c0888c1ebd7ab593d98e8d1a06cc20d9ba96ed
Jonas <jonas.eukan@gmail.com>
2019-10-04 17:14:03 +0800

    Switched to using lists instead of string appending

--- listdir.py
+++ listdir.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import csv
 
 
 def listdir(directory, recursive):
@@ -10,13 +11,13 @@ def listdir(directory, recursive):
     :param recursive: List files recursively if True.
     :return: String containing a list of each file in a directory.
     """
-    csv_output = ""
+    csv_output = []
     file_list = os.listdir(directory)
 
     for file in file_list:
         file_path = f"{directory}/{file}"
         if os.path.isfile(file_path):
-            csv_output += f"{directory},{file},{os.stat(file_path).st_size}\n"
+            csv_output.append((directory, file, os.stat(file_path).st_size))
         elif recursive:
             csv_output += listdir(file_path, recursive)
 
@@ -30,11 +31,12 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--recursive", action="store_true", help="Lists files recursively.")
     args = parser.parse_args()
 
-    with open(f"{args.filename}.csv", "w+") as f:
+    with open(f"{args.filename}.csv", "w+", newline='') as csv_file:
         abs_path = os.path.abspath(args.path)
-        f.write("parent path,filename,file size\n")
+        csv_file.write("parent path,filename,file size\n")
+        writer = csv.writer(csv_filsse, delimiter=',')
 
         try:
-            f.write(listdir(abs_path, args.recursive))
+            writer.writerows(listdir(abs_path, args.recursive))
         except Exception as e:
             print(e)
