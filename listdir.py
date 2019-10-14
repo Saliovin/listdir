373bc5555721a8b6de342d1b729814dd3e3e2637
Jonas <jonas.eukan@gmail.com>
2019-10-14 09:55:41 +0800

    Changed to be recursive by default.

--- listdir.py
+++ listdir.py
@@ -3,12 +3,12 @@ import os
 import csv
 
 
-def listdir(directory, recursive):
+def listdir(directory, nonrecursive):
     """
     Return a string containing a list of each file in a directory. Uses CSV format.
 
     :param directory: Directory to be checked for file listing.
-    :param recursive: List files recursively if True.
+    :param nonrecursive: List files recursively if True.
     :return: String containing a list of each file in a directory.
     """
     csv_output = []
@@ -18,8 +18,8 @@ def listdir(directory, recursive):
         file_path = f"{directory}/{file}"
         if os.path.isfile(file_path):
             csv_output.append((directory, file, os.stat(file_path).st_size))
-        elif recursive:
-            csv_output += listdir(file_path, recursive)
+        elif not nonrecursive:
+            csv_output += listdir(file_path, nonrecursive)
 
     return csv_output
 
@@ -28,15 +28,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="Directory for file listing.")
     parser.add_argument("filename", help="Name of the csv file output.")
-    parser.add_argument("-r", "--recursive", action="store_true", help="Lists files recursively.")
+    parser.add_argument("-n", "--nonrecursive", action="store_true", help="Lists files non-recursively.")
     args = parser.parse_args()
 
     with open(f"{args.filename}.csv", "w+", newline='') as csv_file:
         abs_path = os.path.abspath(args.path)
         csv_file.write("parent path,filename,file size\n")
         writer = csv.writer(csv_file, delimiter=',')
-
-        try:
-            writer.writerows(listdir(abs_path, args.recursive))
-        except Exception as e:
-            print(e)
+        writer.writerows(listdir(abs_path, args.nonrecursive))
